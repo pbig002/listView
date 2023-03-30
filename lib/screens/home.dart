54e300b4bf66ef3535a3f8d49c7ca1796a1b99e0
Mr.Phonpasit Bounkham <newbig6622@gmail.com>
2023-03-30 10:10:45 +0700

    second

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -7,95 +7,54 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-
 class _HomeScreenState extends State<HomeScreen> {
-List<ProductModel> listProduct = [
+  List<ProductModel> listProduct = [
     ProductModel(name: "product1", prize: 100, isActive: true),
     ProductModel(name: "product2", prize: 100, isActive: true),
     ProductModel(name: "product4", prize: 400, isActive: true),
     ProductModel(name: "product3", prize: 300, isActive: true),
   ];
 
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Learning")),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Column(
+      body: ListView(
+        // children:
+        //   [Padding(
+        //     padding: const EdgeInsets.all(8.0),
+        //     child: Column(
+        //       children: [
+        //         Column(
+        //           children: [
+        //             Text("1."),
+        //             Text("product: "),
+        //             Text("prize: 2000 kip"),
+        //             Text("status: isActive"),
+        //             const SizedBox(height: 20),
+        //           ],
+        //         ),
+        //       ],
+        //     ),
+        //   ),
+        // ],
+        children: listProduct.map((pc) {
+          return Card(
+            child: Row(
               children: [
-                Text("1."),
-                Text("product: "),
-                Text("prize: 2000 kip"),
-                Text("status: isActive"),
-                const SizedBox(height: 20),
+                Container(
+                  child: Text(
+                      'product: ${pc.name} prize:${pc.prize} Active:${pc.isActive}'),
+                ),
               ],
             ),
-            Column(
-              children: [
-                Text("1."),
-                Text("product: product1"),
-                Text("prize: 2000 kip"),
-                Text("status: isActive"),
-                const SizedBox(height: 20),
-              ],
-            ),
-            Column(
-              children: [
-                Text("1."),
-                Text("product: product1"),
-                Text("prize: 2000 kip"),
-                Text("status: isActive"),
-                const SizedBox(height: 20),
-              ],
-            ),
-            Column(
-              children: [
-                Text("1."),
-                Text("product: product1"),
-                Text("prize: 2000 kip"),
-                Text("status: isActive"),
-                const SizedBox(height: 20),
-              ],
-            ),
-            Column(
-              children: [
-                Text("1."),
-                Text("product: product1"),
-                Text("prize: 2000 kip"),
-                Text("status: isActive"),
-                const SizedBox(height: 20),
-              ],
-            ),
-                  Column(
-              children: [
-                Text("1."),
-                Text("product: product1"),
-                Text("prize: 2000 kip"),
-                Text("status: isActive"),
-                const SizedBox(height: 20),
-              ],
-            ),
-                  Column(
-              children: [
-                Text("1."),
-                Text("product: product1"),
-                Text("prize: 2000 kip"),
-                Text("status: isActive"),
-                const SizedBox(height: 20),
-              ],
-            ),
-
-            
-          ],
-        ),
+          );
+        }).toList(),
       ),
     );
   }
 }
+
 class ProductModel {
   String name;
   int prize;
